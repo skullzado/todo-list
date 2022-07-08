8ea429912f646e1ca673fe78e8047e37c09ca53d
Paul Calzado <pafcalzado@gmail.com>
2022-07-08 11:40:16 +0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  devServer: {
+    compress: true,
+    hot: true,
+    open: {
+      app: {
+        name: 'chrome',
+      },
+    },
+    port: 3000,
+    static: false,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Project - Todo List',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
