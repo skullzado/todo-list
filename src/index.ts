b7ddcf1f9b9d74840deacc98ce30b3ac16cff3ff
Paul Calzado <pafcalzado@gmail.com>
2022-07-27 11:22:24 +0800

    Deletes code for fresh start

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,1 @@
-import App from './App';
 import './styles.css';
-
-(function () {
-  document.getElementById('root').appendChild(App());
-})();
--- webpack.common.js
+++ webpack.common.js
@@ -3,25 +3,13 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.ts',
-  devtool: 'inline-source-map',
-  devServer: {
-    compress: true,
-    hot: true,
-    open: {
-      app: {
-        name: 'chrome',
-      },
-    },
-    port: 3000,
-    static: false,
-  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Project - Todo List',
       filename: 'index.html',
       template: './src/index.html',
+      favicon: './src/favicon.ico',
       inject: true,
     }),
     new MiniCssExtractPlugin({
@@ -40,8 +28,8 @@ module.exports = {
         use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        test: /\.(png|jpe?g|gif|ico)$/i,
+        use: ['file-loader'],
       },
     ],
   },
@@ -49,7 +37,7 @@ module.exports = {
     extensions: ['.tsx', '.ts', '.js'],
   },
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
